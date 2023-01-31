633a39a794897a9b9f1aa46ec3ae4aeb6b4cf729
Or <73561062+oralush@users.noreply.github.com>
2023-01-31 18:05:03 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from flaskext.mysql import MySQL
+
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_USER', None)
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_PASS', None)
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DB', None)
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_HOST', None)
+mysql = MySQL(app)
+mysql.init_app(app)
+conn = mysql.connect()
+
+
+
 
 @app.route("/")
 def hello():
