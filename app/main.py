5cebdfa0a914ecd25113bebb83a4998d2bae12fe
Or <73561062+oralush@users.noreply.github.com>
2023-01-31 19:07:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flaskext.mysql import MySQL
+import os
 
 app = Flask(__name__)
-
+app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_USER', None)
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_PASS', None)
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DB', None)
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_HOST', None)
+mysql = MySQL(app)
+mysql.init_app(app)
+conn = mysql.connect()
 
 @app.route("/")
-def hello():
-    return "Hello from Python!"
+def main():
+    cursor = conn.cursor()
+    cursor.execute('''SELECT text FROM data WHERE id = 1''')
+    rv = cursor.fetchall()
+    return rv[0]
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
